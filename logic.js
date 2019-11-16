12aa3ac1724fe59498b3eba3d7357b06bb9de4eb
Davey Wong <notwdjw@gmail.com>
2019-11-15 16:59:59 -0800

    Added ajax call to retrieve gifs with button name as query

--- logic.js
+++ logic.js
@@ -22,10 +22,31 @@ $(document).ready(function () {
         else {
             alert("Please add a new instrument")
         }
-
+        $('#gif-input').val('');
         renderButtons(topics);
 
     })
 })
 
+function displayGif() {
+    var gifName = $(this).attr('id')
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=mOcmXHigsqCSutonF7VxoucEEmXp8pCG&q=" + gifName + "&limit=10&rating=&lang=en"
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        for (var i = 0; i < response.data.length; i++) {
+
+            $("#gifs-here").prepend($("<img id='gif' src = '" + response.data[i].images.original.url + "'> <br> <p> Rating: " + (response.data[i].rating).toUpperCase() + "</p>"))
+
+
+        }
+        console.log(response);
+    })
+
+
+
+}
 renderButtons(topics);
+$(document).on("click", ".button", displayGif);
+
