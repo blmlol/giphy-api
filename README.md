698ec6a5b1588fb184f989ce955b1cf5d51e63cb
Davey Wong <notwdjw@gmail.com>
2019-11-16 22:34:33 -0800

    Added an unpause and pause functionality within an onclick listener for the gifs

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # giphy-api
+
+## Installation
+To run the webapp, please download the index.html file and logic.js file and place them in the same folder. To run, open the index.html file.
+
+## Description
+This project is a gif button page. It dynamically adds buttons, and dynamically displays gifs relevant to the button name when the button is clicked.
+The user may add buttons with a name of their choice with the text field and submit button. The gifs dynamically displayed can also be paused and unpaused by clicking on them. 
+
+## Objective
+The objective of the project is to combine HTML, CSS, JavaScript and APIs to create a gif-displaying page. The main focus was on the Jquery ajax method used to request information from online APIs; the API used in this project was the giphy-api. Dynamically moving and creating HTML elements using jQuery methods and event listeners was also implemented in this project. The user interaction with the webpage is through the dynamically generated buttons, and the dynamically displayed gifs.
+
+## Langauges Used
+1. HTML
+2. CSS
+3. JavaScript
+4. jQuery
+
+
+## Technologies Used
+1. Visual Studio Code
+2. Git
+3. GitHub
+4. APIs
+
+
+## License
+[MIT](https://chosealicense.com/licenses/mit/)
--- logic.js
+++ logic.js
@@ -1,6 +1,7 @@
 // The topic will be instruments
 var topics = ['cello', 'violin', 'bass', 'piano', 'guitar', 'ukulele', 'banjo', 'trumpet', 'saxophone'];
 
+//We need a function to render the buttons and display them on the page; we have a set number of buttons on page start from the above array
 function renderButtons(array) {
     $('#buttons').empty();
     for (var i = 0; i < array.length; i++) {
@@ -9,7 +10,7 @@ function renderButtons(array) {
 
 }
 
-
+//Here we have a simple onclick listener for the add gif button; checks for duplicate instruments
 $(document).ready(function () {
 
     $('#add-gif').on('click', function (event) {
@@ -28,6 +29,7 @@ $(document).ready(function () {
     })
 })
 
+//We need a function to display the gifs to the page from the ajax call
 function displayGif() {
     var gifName = $(this).attr('id')
     var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=mOcmXHigsqCSutonF7VxoucEEmXp8pCG&q=" + gifName + "&limit=10&rating=&lang=en"
@@ -35,18 +37,33 @@ function displayGif() {
         url: queryURL,
         method: "GET"
     }).then(function (response) {
+        //Loop through the response data and dynamically prepend gifs to the span gifs-here
         for (var i = 0; i < response.data.length; i++) {
 
-            $("#gifs-here").prepend($("<img id='gif' src = '" + response.data[i].images.original.url + "'> <br> <p> Rating: " + (response.data[i].rating).toUpperCase() + "</p>"))
+            $("#gifs-here").prepend($("<img id='gif' data-still='" + response.data[i].images.original_still.url + "' " + "data-state='" + "still' " + "data-animate='" + response.data[i].images.original.url + "'" + "src = '" + response.data[i].images.original_still.url + "'> <br> <p> Rating: " + (response.data[i].rating).toUpperCase() + "</p>"))
 
 
         }
         console.log(response);
+
     })
 
 
 
 }
+//Here we have an onclick event listener to see if the gif has been clicked on or not
+$(document).on("click", '#gif', function () {
+    //We check the state of the gif to see if it is still or animated (When we dynamically display the gif, it's default state is still)
+    var state = $(this).attr("data-state");
+    if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+    } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+    }
+});
+
 renderButtons(topics);
 $(document).on("click", ".button", displayGif);
 
